1e02dc53ae6330dac7d76d10208bab019e9e9adc
Adam Abed <adam.abedrabboh@gmail.com>
2024-08-25 15:39:09 -0400

    built the classic user interface and the functions needed for the calculator to function

---
+++ index.py
@@ -0,0 +1,18 @@
+import tkinter as tk
+
+calculation = ""
+
+def add_to_calculation(symbol):
+    pass
+
+def solve_calculations():
+    pass
+
+def clr_display():
+    pass
+
+
+#building classic user interface (tk)
+root = tk.Tk()
+root.geometry("500x475")
+root.mainloop()
